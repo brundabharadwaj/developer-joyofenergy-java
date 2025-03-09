1f9eb8be3fff7944d359a9a8b9bace32ca0ee355
Brunda M Bharadwaj <brundabharadwaj22@gmail.com>
2025-03-10 00:13:37 +0530

    code cleanup

--- src/main/java/uk/tw/energy/generator/ElectricityReadingsGenerator.java
+++ src/main/java/uk/tw/energy/generator/ElectricityReadingsGenerator.java
@@ -7,23 +7,23 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import uk.tw.energy.domain.ElectricityReading;
 
 public class ElectricityReadingsGenerator {
 
     public List<ElectricityReading> generate(int number) {
-        List<ElectricityReading> readings = new ArrayList<>();
         Instant now = Instant.now();
 
-        Random readingRandomiser = new Random();
-        for (int i = 0; i < number; i++) {
-            double positiveRandomValue = Math.abs(readingRandomiser.nextGaussian());
-            BigDecimal randomReading = BigDecimal.valueOf(positiveRandomValue).setScale(4, RoundingMode.CEILING);
-            ElectricityReading electricityReading = new ElectricityReading(now.minusSeconds(i * 10L), randomReading);
-            readings.add(electricityReading);
-        }
-
-        readings.sort(Comparator.comparing(ElectricityReading::time));
-        return readings;
+        return IntStream.range(0, number)
+                .mapToObj(i -> new ElectricityReading(
+                        now.minusSeconds(i * 10L),
+                        BigDecimal.valueOf(Math.abs(ThreadLocalRandom.current().nextGaussian()))
+                                .setScale(4, RoundingMode.CEILING)))
+                .collect(Collectors.toList());
     }
+
 }
